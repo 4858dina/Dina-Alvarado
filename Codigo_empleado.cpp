096d081d882737a97b15d88f4068c3ced1f00ac8
4858dina <90992162+4858dina@users.noreply.github.com>
2021-09-18 20:45:21 -0600

    Add files via upload

---
+++ Codigo_empleado.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string nom;
+	int edad;
+	float sueldo;
+	
+	cout<<"****Datos del empleado ****"<<endl;
+	cout<<"Ingrese el nombre: ";
+	cin>>nom;
+	cout<<"Ingrese la edad: ";
+	cin>>edad;
+	cout<<"Ingrese el sueldo: ";
+	cin>>sueldo;
+	cout<<endl;
+	
+	cout<<"El nombre es: "<<nom<<endl;
+	cout<<"La edad es: "<<edad<<endl;
+	cout<<"El sueldo es: "<<sueldo<<endl;
+
+	
+	
+	}
